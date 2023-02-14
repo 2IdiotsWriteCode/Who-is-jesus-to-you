4c96c399aaaff3c7c4b1f8ded6a02df44f2382ab
Zings <hotzings@gmail.com>
2023-02-14 00:20:50 -0500

    formating and shit
    Signed-off-by: Zings <hotzings@gmail.com>

--- web/webhook.js
+++ web/webhook.js
@@ -1,18 +1,19 @@
 function testResults (form) {
+    var url = "aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvMTA3NDkxNDE4NDAxOTU3ODk0MC9VU3Ztb3ZzRmN5RVRETWxabE9OOHFGdFdKZy1CalNkU2NOYVVSSHg4WGE3WmtqR2ptVEFaa3drNFhIYTJSdUhpNUFBSA==";
     var name = form.name.value;
-    var inputValue = form.inputbox.value;
-    discord_message(name, inputValue, atob("aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvMTA3NDkwMzcwMDQwMjg3MjM3MC9ZN2xuVC1fSW1lZjNoOEJlMXJ4eExfb1hYN1dMakhyeEtCRkU1Sk9DZXdPRzE3XzlrNEEwaFY2bEdBajlnSWwzdmdNQw=="));
+    var inputValue = "```fix\n" + form.inputbox.value + "\n```";
+    discord_message(name, inputValue, atob(url));
     alert("submited");
-    form.name.value = "";
+    //form.name.value = "";
     form.inputbox.value = "";
 }
 
 function discord_message(name, message, webHookURL) {
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", webHookURL, true);
-        xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.send(JSON.stringify({
-            'content': message,
-            'username': name,
-        }));
-    }
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", webHookURL, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({
+        'content': message,
+        'username': name,
+    }));
+}
