83a66b0873678ad515f600b8c7019d64cd3d0353
Zings <hotzings@gmail.com>
2023-02-16 21:55:01 -0500

    did some style shit lol
    Signed-off-by: Zings <hotzings@gmail.com>

--- web/webhook.js
+++ web/webhook.js
@@ -1,11 +1,12 @@
 function testResults (form) {
     var url = "aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvMTA3NDkxNDE4NDAxOTU3ODk0MC9VU3Ztb3ZzRmN5RVRETWxabE9OOHFGdFdKZy1CalNkU2NOYVVSSHg4WGE3WmtqR2ptVEFaa3drNFhIYTJSdUhpNUFBSA==";
     var name = form.name.value;
-    var inputValue = "```fix\n" + form.inputbox.value + "\n```";
-    discord_message(name, inputValue, atob(url));
-    alert("submited");
-    //form.name.value = "";
-    form.inputbox.value = "";
+    var content = "```fix\nWho is jesus to you:\n" + form.WhoIsJesusToYouBox.value + "\n```";
+    content = content + "```fix\nquestion2:\n" + form.Q2.value + "```";
+    content = content + "```fix\nquestion3:\n" + form.Q3.value + "```";
+    discord_message(name, content, atob(url));
+    //alert("submited");
+    //form.inputbox.value = "";
 }
 
 function discord_message(name, message, webHookURL) {
@@ -17,5 +18,3 @@ function discord_message(name, message, webHookURL) {
         'username': name,
     }));
 }
-
-//test test test
